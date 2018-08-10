67e5c2a537823c3121ee4d581fcb4b978848300a
Codeesar <32211521+Codeesar@users.noreply.github.com>
2018-08-10 04:49:50 +0000

    The main script

---
+++ mail_spammer.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @param string $mail = 'example@gmail.com'
+ * @param string $title = 'SPAM!'
+ * @param string $text = 'SPAM'
+ * @param int $count = 10
+ *
+ * @return boolean
+ */
+function mailSpam(string $mail = 'example@gmail.com', string $title = 'SPAM!', string $text = 'SPAM', int $count = 10) : bool{
+	while($count > 0){
+		mail($mail, $title, $text);
+		$count--;
+		}
+		return true;
+	}
+?>
