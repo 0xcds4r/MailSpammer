54b470f4a4c6798d35aca03b49a043500f68b09f
kefs <32211521+Codeesar@users.noreply.github.com>
2019-08-27 15:17:48 +1000

    Update mail_spammer.php

--- mail_spammer.php
+++ mail_spammer.php
@@ -2,20 +2,24 @@
 
 declare(strict_types=1);
 
-/**
+/*
  * @param string $mail = 'example@gmail.com'
  * @param string $title = 'SPAM!'
  * @param string $text = 'SPAM'
  * @param int $count = 10
+ * @param int $delay = 2000
  *
  * @return boolean
  */
-function mailSpam(string $mail = 'example@gmail.com', string $title = 'SPAM!', string $text = 'SPAM', int $count = 10) : bool{
-	while($count > 0){
-		mail($mail, $title, $text);
-		$count--;
-	}
-	return true;
+function mailSpam(string $mail = 'example@gmail.com', string $title = 'SPAM!', string $text = 'SPAM', int $count = 10, int $delay = 2000) : bool
+{
+    while($count > 0){
+        mail($mail, $title, $text);
+	$count--;
+	
+	sleep($delay);
+    }
+    return true;
 }
 
 // Using: mailSpam(...args)
